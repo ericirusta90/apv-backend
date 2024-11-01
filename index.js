68c14d5a8b1883e0388aa9348fa29ba7c8cce71d
ericirusta90 <ericirusta90@gmail.com>
2024-11-01 17:04:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import conectarDB from "./config/db.js";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacientesRoutes from "./routes/pacienteRoutes.js";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+conectarDB();
+
+const dominioisPermitidos= [process.env.FRONTEND_URL];
+
+const corsOptions= {
+    origin: function (origin, callback) {
+        if (dominioisPermitidos.indexOf(origin) !== -1 ) {
+            //el origen del request esta permitido
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
+
+app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/pacientes", pacientesRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`servidor funcionando en el puerto: ${PORT}`);
+});
