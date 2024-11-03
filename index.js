4d9496e2523a5980ecdfae21ccd74fd5ec9da28e
ericirusta90 <ericirusta90@gmail.com>
2024-11-02 21:14:53 -0300

    fix

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const corsOptions= {
     }
 }
 
-app.use(cors(corsOptions))
+//app.use(cors( corsOptions ))
+app.use(cors( {origin: '*'}));
 
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacientesRoutes);
